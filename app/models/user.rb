a25d99567ad2e8539f4ca131de48b5d8c053bd0f
khuram <khuramdogar73@gmail.com>
2019-03-21 14:13:50 +0500

    empty record lets get start with devise token auth

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
-  validates_uniqueness_of :email
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_130647) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.string "authentication_token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
